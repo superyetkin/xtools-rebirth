24d3d03e21ece4f4dcb177d092503c4ec5d145c1
Sam Wilson <sam@samwilson.id.au>
2017-03-30 14:12:51 +0800

    Remove password handling

--- xtools_utilities/meta-db.sh
+++ xtools_utilities/meta-db.sh
@@ -3,14 +3,10 @@ set -x
 
 METADB="xtools_meta"
 echo "Creating database: $METADB"
-PASSARG=""
-if [ -n "$DBPASS" ]; then
-    PASSARG=" -p$DBPASS -proot "
-fi
-mysql "$PASSARG" -e "DROP DATABASE IF EXISTS $METADB"
-mysql "$PASSARG" -e "CREATE DATABASE $METADB"
+mysql -e "DROP DATABASE IF EXISTS $METADB;"
+mysql -e "CREATE DATABASE $METADB;"
 SQLFILE=$(cd $(dirname "$0"); pwd -P)"/meta-db.sql"
 echo "Importing $SQLFILE"
-cat $SQLFILE | mysql "$PASSARG" "$METADB"
+mysql $METADB < "$SQLFILE"
 echo "Inserting data into $METADB"
-mysql "$PASSARG" -e "INSERT INTO $METADB.wiki SET dbname='xtools_wiki1', name='xTools Test Wiki 1', family='testing', url='http://localhost/xtools_wiki1';"
+mysql -e "INSERT INTO $METADB.wiki SET dbname='xtools_wiki1', name='xTools Test Wiki 1', family='testing', url='http://localhost/xtools_wiki1';"
