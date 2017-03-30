7de5410dc6b8db83cbe7ea12025ef1bd4bcce4fa
Sam Wilson <sam@samwilson.id.au>
2017-03-30 09:59:26 +0800

    Travis debugging

--- xtools_utilities/meta-db.sh
+++ xtools_utilities/meta-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 METADB="xtools_meta"
-echo "Creating meta database"
+echo "Creating database: $METADB"
 PASSARG=""
 if [ -n "$DBPASS" ]; then
     PASSARG="-p$DBPASS"
@@ -9,5 +9,6 @@ fi
 mysql "$PASSARG" -uroot -e "DROP DATABASE IF EXISTS $METADB"
 mysql "$PASSARG" -uroot -e "CREATE DATABASE $METADB"
 SQLFILE=$(cd $(dirname "$0"); pwd -P)"/meta-db.sql"
+echo "Importing $SQLFILE"
 mysql "$PASSARG" -uroot $METADB < "$SQLFILE"
 mysql "$PASSARG" -uroot -e "INSERT INTO wiki SET dbname='xtools_wiki1', name='xTools Test Wiki 1', family='testing', url='http://localhost/xtools_wiki1';" $METADB
